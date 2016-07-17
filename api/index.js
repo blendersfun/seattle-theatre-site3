5d4bd6bafc3683404aa96eaee817e6498bdbd773
Aaron Moore <two.a.ron@gmail.com>
2016-07-16 18:56:38 -0700

    Added nginx which enabled me to remove cors as it shouldn't be needed at this stage.

--- api/index.js
+++ api/index.js
@@ -1,10 +1,7 @@
 var express = require('express');
-var cors = require('cors');
 
 var app = express();
 var test = require('./mysql-test');
-
-app.use(cors());
  
 app.get('/', function (req, res) {
     test(function (err, rows) {
